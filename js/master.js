90b69946d0cb109e1bafb6bd470056c42c7896d1
kabaiss <aissakabot@gmail.com>
2020-05-05 00:24:48 +0200

    add switching color in options-box

--- js/master.js
+++ js/master.js
@@ -6,6 +6,16 @@ this.classList.toggle("fa-spin")
 document.querySelector(".settings-box").classList.toggle("open")
 } 
 
+
+//Swirth colors
+const colorsLi=document.querySelectorAll(".colors-list li");
+colorsLi.forEach(li=>{
+    //click on every list item
+  li.addEventListener("click",(e)=>{
+    //console.log(e.target.dataset.color)
+    document.documentElement.style.setProperty("--main-color",e.target.dataset.color)
+  })
+})
 let landingpage=document.querySelector(".landing-page")
 let imgarray=["01.jpg","08.jpg","03.jpg","05.jpg","06.jpg","07.jpg"]
 
